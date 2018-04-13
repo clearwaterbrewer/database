f48edff4994bfdf4f797b31cfeff02e8642fa32b
michael cotherman <mjcother@yahoo.com>
2018-04-13 02:38:58 +0000

    initial commit

---
+++ CDCbatches.php
@@ -0,0 +1,62 @@
+<?php
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+
+sec_session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Batches</title>
+        <link rel="stylesheet" href="styles/main.css" />
+    </head>
+    <body>
+        <?php if (login_check($mysqli) == true) : ?>
+        <p>Logged in as: <?php echo htmlentities($_SESSION['username']); ?></p>
+            <p>
+                Cotherman Distilling Co. <br>
+		Last 5 Batches 
+
+
+		<?php
+		$sql="SELECT * FROM (SELECT * FROM Batches ORDER BY BatchNum DESC LIMIT 5) sub ORDER BY BatchNum ASC";
+		$result = $mysqli->query($sql);
+
+		echo "<table>
+		<tr>
+		<th>Batch</th>
+		<th>Name</th>
+		<th>Class</th>
+		<th>Source Product</th>
+		<th>Source Ingredient</th>
+		</tr>";
+
+		if ($result->num_rows > 0) {
+
+		  while ($row = $result->fetch_assoc()) {
+		    echo "<tr>";
+		    echo "<td>" . $row['BatchNum'] . "</td>";
+		    echo "<td>" . $row['BatchName'] . "</td>";
+		    echo "<td>" . $row['Class'] . "</td>";
+		    echo "<td>" . $row['SourceProduct'] . "</td>";
+		    echo "<td>" . $row['SourceIngredient'] . "</td>";
+		    echo "</tr>";
+		  }
+		}
+
+
+		echo "</table>";
+		?>
+
+		<br>******NEW BATCH BUTTON******
+            </p>
+	    <p><a href="includes/logout.php">logout</a></p>
+            <p>Return to <a href="protected_page.php">home page</a></p>
+        <?php else : ?>
+            <p>
+                <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+            </p>
+        <?php endif; ?>
+    </body>
+</html>
---
+++ includes/psl-config.php
@@ -0,0 +1,12 @@
+<?php
+
+define("HOST", "localhost"); 			// The host you want to connect to. 
+define("USER", "DBadmin"); 			// The database username. 
+define("PASSWORD", "qwer1234"); 	// The database password. 
+define("DATABASE", "CDCtest");             // The database name.
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE);    // For development purposes only!!!!
+
