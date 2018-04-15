ddce353ef94ce2aa0ea58bc283ab3f86c5302c6b
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-15 15:24:36 -0400

    Update CDCbatches.php

--- CDCbatches.php
+++ CDCbatches.php
@@ -46,7 +46,7 @@ sec_session_start();
 		$result = $mysqli->query($sql);
 		if ($result->num_rows > 0) {
 		  while ($row = $result->fetch_assoc()) {
-    			echo "<div/>";
+    			echo '<div class=row/>';
 			echo $row['BatchNum'];
       			echo "<//div/>";
 		  }
