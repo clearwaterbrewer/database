a22026078e09a85b899c4d930fc475d9e9f2897e
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 13:15:36 -0400

    Update CDC_BatchForm.php

--- CDC_BatchForm.php
+++ CDC_BatchForm.php
@@ -34,9 +34,9 @@ sec_session_start();
         </div>
     </li>
     <li class="form-line" data-type="control_dropdown">
-        <label class="form-label form-label-top form-label-auto" for="Class">Class</label>
+        <label class="form-label form-label-top form-label-auto" for="ClassType">ClassType</label>
         <div class="form-input-wide">
-          <select required class="form-dropdown" name="Class" data-component="dropdown">
+          <select required class="form-dropdown" name="ClassType" data-component="dropdown">
             <option value="">  </option>
             <option value="Whisky 160 and under"> Whisky 160 and under </option>
             <option value="Rum"> Rum </option>
