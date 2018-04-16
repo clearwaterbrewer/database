2bcfd488bd07efb579827ff6d896a044adb268d6
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 13:18:11 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -1,8 +1,8 @@
 <?php
 include_once 'db_connect.php';
 include_once 'psl-config.php';
-if ($insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, Class, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)")) {
-        $insert_stmt->bind_param($BatchName, $Class, $SourceProduct, $SourceIngredient);
+if ($insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, ClassType, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)")) {
+        $insert_stmt->bind_param($BatchName, $ClassType, $SourceProduct, $SourceIngredient);
             // Execute the prepared query.
             if (! $insert_stmt->execute()) {
                 header('Location: ../error.php?err=Batch addition failure: INSERT');
