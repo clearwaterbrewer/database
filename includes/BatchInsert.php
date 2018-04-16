d82675fd87d555a79515b945b9671a6e7d881299
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 12:24:01 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -1,23 +1,14 @@
 <?php
 include_once 'db_connect.php';
 include_once 'psl-config.php';
-if (isset($_POST['BatchName'], $_POST['Class'], $_POST['SourceProduct'], $_POST['SourceIngredient'])) {
-    // Sanitize and validate the data passed in
-    $BatchName = filter_input(INPUT_POST, 'BatchName', FILTER_SANITIZE_STRING);
-    $Class = filter_input(INPUT_POST, 'Class', FILTER_SANITIZE_STRING);
-    $SourceProduct = filter_input(INPUT_POST, 'SourceProduct', FILTER_SANITIZE_STRING);
-    $SourceIngredient = filter_input(INPUT_POST, 'SourceIngredient', FILTER_SANITIZE_STRING);
-
-    // Insert the new Batch into the database 
-    if ($insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, Class, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)")) {
+if ($insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, Class, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)")) {
         $insert_stmt->bind_param($BatchName, $Class, $SourceProduct, $SourceIngredient);
             // Execute the prepared query.
             if (! $insert_stmt->execute()) {
                 header('Location: ./error.php?err=Batch addition failure: INSERT');
                 exit();
-            }
-        }
-        header('Location: ./CDCbatches.php');
-        exit();
-    }
+                }
+       }
+header('Location: ./CDCbatches.php');
+exit();
 }
