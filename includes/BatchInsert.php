58ab340161f647c9ada4e99376a0f366362a310f
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 15:25:27 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -1,14 +1,21 @@
 <?php
 include_once 'db_connect.php';
 include_once 'psl-config.php';
-if (isset($_POST['submit'])) {
-  $BatchName = $_POST['BatchName'];
-  $ClassType = $_POST['ClassType'];
-  $SourceProduct = $_POST['SourceProduct'];
-  $SourceIngredient = $_POST['SourceIngredient'];
+
+
+$sql = "INSERT INTO persons (BatchName, ClassType, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)";
+if($stmt = $mysqli->prepare($sql)){
+    $stmt->bind_param("ssss", $BatchName, $ClassType, $SourceProduct, $SourceIngredient);
+    $BatchName = $_POST['BatchName'];
+    $ClassType = $_POST['ClassType'];
+    $SourceProduct = $_POST['SourceProduct'];
+    $SourceIngredient = $_POST['SourceIngredient'];
+    $stmt->execute();
         
-  $insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, ClassType, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)");
-  $insert_stmt->bind_param("ssss", $BatchName, $ClassType, $SourceProduct, $SourceIngredient);
-  $insert_stmt->execute());
-}
+    echo "Records inserted successfully.";
+    } 
+else{
+    echo "ERROR: Could not prepare query: $sql. " . $mysqli->error;
+    }
+ 
 ?>
