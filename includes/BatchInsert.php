f77bf3df3515cc6c1579435122f78d5c9aabeb1a
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 10:57:06 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -2,12 +2,12 @@
 include_once 'db_connect.php';
 include_once 'psl-config.php';
 $error_msg = "";
-if (isset($_POST['BatchName'], $_POST['Class'], $_POST['SourceProduct'], $_POST['SourceIngredient'])) {
+if (isset($_POST['BatchName1'], $_POST['Class1'], $_POST['SourceProduct1'], $_POST['SourceIngredient1'])) {
     // Sanitize and validate the data passed in
-    $BatchName = filter_input(INPUT_POST, 'BatchName', FILTER_SANITIZE_STRING);
-    $Class = filter_input(INPUT_POST, 'Class', FILTER_SANITIZE_STRING);
-    $SourceProduct = filter_input(INPUT_POST, 'SourceProduct', FILTER_SANITIZE_STRING);
-    $SourceIngredient = filter_input(INPUT_POST, 'SourceIngredient', FILTER_SANITIZE_STRING);
+    $BatchName = filter_input(INPUT_POST, 'BatchName1', FILTER_SANITIZE_STRING);
+    $Class = filter_input(INPUT_POST, 'Class1', FILTER_SANITIZE_STRING);
+    $SourceProduct = filter_input(INPUT_POST, 'SourceProduct1', FILTER_SANITIZE_STRING);
+    $SourceIngredient = filter_input(INPUT_POST, 'SourceIngredient1', FILTER_SANITIZE_STRING);
 
     // Insert the new Batch into the database 
     if ($insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, Class, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)")) {
