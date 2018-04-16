9089c237d9d4d28c3e18a7e7d0b3f5cc5d97b292
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 15:15:18 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -2,7 +2,10 @@
 include_once 'db_connect.php';
 include_once 'psl-config.php';
 if (isset($_POST['submit'])) {
-        
+  $BatchName = $_POST['BatchName'];
+  $ClassType = $_POST['ClassType'];
+  $SourceProduct = $_POST['SourceProduct'];
+  $SourceIngredient = $_POST['SourceIngredient'];
         
   $insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, ClassType, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)");
   $insert_stmt->bind_param("ssss", $BatchName, $ClassType, $SourceProduct, $SourceIngredient);
