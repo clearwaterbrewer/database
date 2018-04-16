50719eaabec254ffeffb31d6604efb2b5507c7fb
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 15:05:55 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])) {
         
         
   $insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, ClassType, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)");
-  $insert_stmt->bind_param('ssss', $BatchName, $ClassType, $SourceProduct, $SourceIngredient);
+  $insert_stmt->bind_param("ssss", $BatchName, $ClassType, $SourceProduct, $SourceIngredient);
   $insert_stmt->execute());
 }
 ?>
