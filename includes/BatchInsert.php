d10a9422e16d5d08278d84e7a8da4a7b9c781d22
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 10:16:43 -0400

    Create BatchInsert.php

---
+++ includes/BatchInsert.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'db_connect.php';
+include_once 'psl-config.php';
+$error_msg = "";
+if (isset($_POST['BatchName'], $_POST['Class'], $_POST['SourceProduct'], $_POST['SourceIngredient'])) {
+    // Sanitize and validate the data passed in
+    $BatchName = filter_input(INPUT_POST, 'BatchName', FILTER_SANITIZE_STRING);
+    $Class = filter_input(INPUT_POST, 'Class', FILTER_SANITIZE_STRING);
+    $SourceProduct = filter_input(INPUT_POST, 'SourceProduct', FILTER_SANITIZE_STRING);
+    $SourceIngredient = filter_input(INPUT_POST, 'SourceIngredient', FILTER_SANITIZE_STRING);
+
+    // Insert the new Batch into the database 
+    if ($insert_stmt = $mysqli->prepare("INSERT INTO Batches (BatchName, Class, SourceProduct, SourceIngredient) VALUES (?, ?, ?, ?)")) {
+        $insert_stmt->bind_param('ssss', $BatchName, $Class, $SourceProduct, $SourceIngredient);
+            // Execute the prepared query.
+            if (! $insert_stmt->execute()) {
+                echo "Error at BatchInsert.php";
+                exit();
+            }
+        }
+        echo "Data Submitted succesfully";
+        exit();
+    }
+}
