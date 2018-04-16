7519615ce7b0b214c7b16ac84f8ebc4a0fed3b02
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 09:45:48 -0400

    Update CDC_BatchForm2.php

--- CDC_BatchForm2.php
+++ CDC_BatchForm2.php
@@ -4,8 +4,7 @@ include_once 'includes/functions.php';
 sec_session_start();
 ?>
 <!DOCTYPE HTML>
-<html>
-<head>
+<html class="supernova"><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta property="og:title" content="CDC Batch Entry" >
 <meta property="og:description" content="Please click the link to complete this form.">
@@ -13,29 +12,11 @@ sec_session_start();
 <meta name="HandheldFriendly" content="true" />
 <title>CDC Batch Entry</title>
 <link type="text/css" rel="stylesheet" href="styles/main.css" />
-<link type="text/css" rel="stylesheet" href="bootstrap.min.css" />
-<style type="text/css" id="form-designer-style">
-.form-label.form-label-auto { display: block; float: none; text-align: left; width: inherit; } 
-</style>
-<style>
-#loading-img{display:none;}
-
-.response_msg{
-margin-top:10px;
-font-size:13px;
-background:#E5D669;
-color:#ffffff;
-width:250px;
-padding:3px;
-display:none;
-}
-</style>
-
 </head>
+
 <body>
 <?php if (login_check($mysqli) == true) : ?>
-<form class="jotform-form" accept-charset="utf-8" id="BatchForm" name="batchform" action="" method="post">
-
+<form class="jotform-form" accept-charset="utf-8" id="BatchForm" name="batchform" action="CDC_BatchForm3.php" method="post">
 <div class="form-all">
   <ul class="form-section page-section">
     <li id="cid_1" class="form-input-wide" data-type="control_head">
@@ -46,20 +27,18 @@ display:none;
         </div>
       </div>
     </li>
-
     <li class="form-line" data-type="control_textbox">
         <label class="form-label form-label-top form-label-auto" for="BatchName">Batch Name</label>
         <div class="form-input-wide">
-          <input type="text" class="form-control" name="BatchName" Placeholder="Rum 13" required>
+          <input type="text" class="form-control" size="20" name="BatchName" value="" Placeholder="Rum 13" required>
         </div>
     </li>
- 
     <li class="form-line" data-type="control_dropdown">
         <label class="form-label form-label-top form-label-auto" for="Class">Class</label>
         <div class="form-input-wide">
-          <select required class="form-dropdown" name="Class" style="width:150px" data-component="dropdown">
+          <select required class="form-dropdown" name="Class" data-component="dropdown">
             <option value="">  </option>
-            <option value="Whiskey 160"> Whiskey 160</option>
+            <option value="Whisky 160 and under"> Whisky 160 and under </option>
             <option value="Rum"> Rum </option>
             <option value="Gin"> Gin </option>
             <option value="Vodka"> Vodka </option>
@@ -68,25 +47,22 @@ display:none;
           </select>
         </div>
     </li>
-  
     <li class="form-line" data-type="control_textbox">
-        <label class="form-label form-label-top form-label-auto" for="SourceProduct">Source Product</label>
+        <label class="form-label form-label-top form-label-auto" id="label_6" for="SourceProduct">Source Product</label>
         <div class="form-input-wide">
-          <input type="text" class="form-control" name="SourceProduct" Placeholder="Molasses" required>
+          <input type="text" class="form-control" size="20" name="SourceProduct" value="" Placeholder="Molasses" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
-        <label class="form-label form-label-top form-label-auto" for="SourceIngredient">Source Ingredient</label>
+        <label class="form-label form-label-top form-label-auto" id="label_7" for="SourceIngredient">Source Ingredient</label>
         <div class="form-input-wide">
-          <input type="text" class="form-control" name="SourceIngredient" Placeholder="Molasses" required>
+          <input type="text" class="form-control" size="20" name="SourceIngredient" value="" Placeholder="Molasses" required>
         </div>
     </li>
-
-
-    <li class="form-line" data-type="control_button">
-        <div class="form-input-wide">
+    <li class="form-line" data-type="control_button" id="id_8">
+        <div id="cid_8" class="form-input-wide">
           <div style="margin-left:40px" class="form-buttons-wrapper">
-            <button type="submit" class="btn btn-primary" name="submit" value="Submit" id="submit_form">Submit</button>
+            <button type="submit" class="form-submit-button" name="submit" value="Submit" id="submit_form">Submit</button>
           </div>
        </div>
     </li>
@@ -114,41 +90,7 @@ display:none;
     </li>
   </ul>
 </div>
-       
-<div class="response_msg"></div>
-</div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-$("#contact-form").on("submit",function(e){
-e.preventDefault();
-if($("#contact-form [name='BatchName']").val() === '')
- {$("#contact-form [name='BatchName']").css("border","1px solid red");}
-else
-{$("#loading-img").css("display","block");
-  var sendData = $( this ).serialize();
-  $.ajax({type: "POST", url: "includes/get_response.php", data: sendData, success: function(data){
-    $("#loading-img").css("display","none");
-    $(".response_msg").text(data);
-    $(".response_msg").slideDown().fadeOut(3000);
-    $("#contact-form").find("input[type=text], input[type=email], textarea").val("");
-  }
-});
-}
-});
-
-$("#contact-form input").blur(function(){
-var checkValue = $(this).val();
-if(checkValue != '')
-{
-$(this).css("border","1px solid #eeeeee");
-}
-});
-});
-</script>
-</form> 
-
-  
+</form>          
 <?php else : ?>
 <form class="jotform-form" accept-charset="utf-8">
   <div class="form-all">
@@ -182,5 +124,3 @@ $(this).css("border","1px solid #eeeeee");
 <?php endif; ?>
 </body>
 </html>
-
-
