1aaecb4380f70818c6377667d72c2b3a5012973b
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-16 15:28:49 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -12,7 +12,7 @@ if($stmt = $mysqli->prepare($sql)){
     $SourceIngredient = $_POST['SourceIngredient'];
     $stmt->execute();
         
-    header('Location: ./CDCbatches.php');
+    header('Location: ../CDCbatches.php');
     } 
 else{
     echo "ERROR: Could not prepare query: $sql. " . $mysqli->error;
