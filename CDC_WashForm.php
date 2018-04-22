94b1e7a4b0c9a1f04a95757121e5ac3871777117
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-22 11:25:17 -0400

    Update CDC_WashForm.php

--- CDC_WashForm.php
+++ CDC_WashForm.php
@@ -51,7 +51,7 @@ sec_session_start();
     <li class="form-line" data-type="control_textbox">
         <label class="form-label form-label-top form-label-auto" for="SourceIngredient">Source Ingredient</label>
         <div class="form-input-wide">
-          <input type="text" class="form-control" size="20" name="SourceIngredient" value=<?php echo $row['Source Ingredient'] ?> required>
+          <input type="text" class="form-control" size="20" name="SourceIngredient" value=<?php echo $row['SourceIngredient'] ?> required>
         </div>
     </li>
   </ul>
