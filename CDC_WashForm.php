db8ee3b39e79372733b8e35ed39bf9b7d2a74d4f
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2018-04-25 05:25:44 -0400

    Update CDC_WashForm.php

--- CDC_WashForm.php
+++ CDC_WashForm.php
@@ -45,6 +45,13 @@ sec_session_start();
             echo "<option value='" . $row['BatchNum'] . "'>" . $row['BatchNum'] . "</option>";
           }
           echo "</select>";
+          $batchinfo = "SELECT * FROM Batches WHERE $BatchNum = "..$BatchNum;
+          $batchresult = $mysqli->query($batchinfo);
+          $batchrow = $batchresult->fetch_assoc();
+          $ThisBatchName = $batchrow['BatchName'];
+          $ThisBatchSP = $batchrow['SourceProduct'];
+          $ThisBatchSI = $batchrow['SourceIngredient'];
+          
           ?>
       </div>
     </li>
