66c71dd895452fcf5d6f44f15f8e99f6b7ea781f
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-17 10:41:53 -0400

    Update CDC_RemoveFromBond.php

--- CDC_RemoveFromBond.php
+++ CDC_RemoveFromBond.php
@@ -46,7 +46,7 @@ sec_session_start();
 	</div>
 <?php 
       	  $sql="SELECT * FROM 
-		 (SELECT * FROM WashRuns ORDER BY DateTimeCode DESC LIMIT 30) 
+		 (SELECT * FROM RemovedFromBond ORDER BY DateTimeCode DESC LIMIT 30) 
 		  sub ORDER BY DateTimeCode DESC";
 		$result = $mysqli->query($sql);
 		if ($result->num_rows > 0) {
