b7a58636f5e33ae479ca4d8464093178b3a1cc7e
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-17 16:58:05 -0400

    Update CDC_RemoveFromBond2.php

--- CDC_RemoveFromBond2.php
+++ CDC_RemoveFromBond2.php
@@ -48,8 +48,8 @@ sec_session_start();
 	  <div class="column">Notes</div>
 	</div>
 <?php 
-$sql="SELECT * FROM (SELECT * FROM RemovedFromBond ORDER BY DateTimeCode DESC LIMIT 30) 
-	sub INNER JOIN Batches ON RemovedFromBond.BatchNum=Batches.BatchNum ORDER BY DateTimeCode DESC";
+$sql="SELECT * FROM (SELECT * FROM RemovedFromBond ORDER BY DateTimeCode DESC LIMIT 30 INNER JOIN Batches ON RemovedFromBond.BatchNum=Batches.BatchNum) 
+	sub ORDER BY DateTimeCode DESC";
 	$result = $mysqli->query($sql);
 	if ($result->num_rows > 0) {
 	  while ($row = $result->fetch_assoc()) {
