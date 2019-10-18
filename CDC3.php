1d0c0b9dfaa0d1afc0cbf3b381a421a554d95da4
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-18 15:39:08 -0400

    Update CDC3.php

--- CDC3.php
+++ CDC3.php
@@ -4,6 +4,22 @@ include_once 'includes/db_connect.php';
 <html>
 <head>
 <script>
+function showBatch(str) {
+    if (str == "") {
+        document.getElementById("txtMessage").innerHTML = "";
+        return;
+    } else { 
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtMessage").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","CDCAjax3.php?b="+str,true);
+        xmlhttp.send();
+    }
+}
+    
 function showContainer(str) {
     if (str == "") {
         document.getElementById("txtMessage").innerHTML = "";
@@ -36,6 +52,16 @@ function showContainer(str) {
           echo "</select>";
     ?>
 
+    <?php
+          $sql = "SELECT * FROM Batches ORDER by BatchNum DESC";
+          $result = $mysqli->query($sql);
+          echo "<select id='BatchNum' name='BatchNum' onchange='showBatch(this.value)'>";
+          echo "<option value=''> Select a Batch        </option>";
+          while ($row = $result->fetch_assoc()) {
+            echo "<option value='" . $row['BatchNum'] .  "'>" . $row['BatchNum'] .' - '. $row['BatchName'] . "</option>";
+          }
+          echo "</select>";
+    ?>
 
 
 </form>
