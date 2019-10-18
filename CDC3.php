5aa6c2a8ed075b5152ef260ec16827eccf25e3c1
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-18 09:35:50 -0400

    Update CDC3.php

--- CDC3.php
+++ CDC3.php
@@ -39,16 +39,30 @@ function showHint(str_showHint) {
 <body>
 
 <form>
-          <?php
-          $sql = "SELECT * FROM Containers ORDER by ContainerName DESC";
-          $result = $mysqli->query($sql);
-          echo "<select class='form-dropdown' required name='id' onchange='showContainer(this.value)'>";
-          echo "<option value=''> Select a Container        </option>";
-          while ($row = $result->fetch_assoc()) {
-            echo "<option value='" . $row['id'] .  "'>" . $row['ContainerName'] .' - '. $row['ContainerVolume'] . "</option>";
-          }
-          echo "</select>";
-          ?>
+          <select name="ID" onchange="showContainer(this.value)">
+  <option value="">Select a Container:</option>
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
+  <option value="7">7</option>
+  <option value="8">8</option>
+  <option value="9">9</option>
+  <option value="10">10</option>
+  <option value="11">11</option>
+  <option value="12">12</option>
+  <option value="13">13</option>
+  <option value="14">14</option>
+  <option value="15">15</option>
+  <option value="16">16</option>
+  <option value="17">17</option>
+  <option value="18">18</option>
+  <option value="19">19</option>
+  <option value="20">20</option>
+  </select>
+
 
 </form>
 <br>
