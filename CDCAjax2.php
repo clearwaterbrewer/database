664ae17ddccd4b4ca5b995e02376695b8a31285c
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-18 08:28:25 -0400

    Update CDCAjax2.php

--- CDCAjax2.php
+++ CDCAjax2.php
@@ -1,5 +1,7 @@
 <?php
 include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+sec_session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,7 +21,8 @@ include_once 'includes/db_connect.php';
   </style>
  </head>
  <body>
- 
+
+  <?php if (login_check($mysqli) == true) : ?>
   <?php
   $q = intval($_GET['q']);
 
@@ -96,6 +99,36 @@ if ($r !== "") {
 echo $hint === "" ? "no suggestion" : $hint;
 ?>
 
-
+<?php else : ?>
+<form class="jotform-form" accept-charset="utf-8">
+  <div class="form-all">
+    <ul class="form-section page-section">        
+      <li id="cid_1" class="form-input-wide" data-type="control_head">
+        <div class="form-header-group ">
+          <div class="header-text httal htvam">
+            <h2 id="header_1" class="form-header" data-component="header">
+              NOT AUTHORIZED
+             </h2>
+              <label class="form-label form-label-top form-label-auto">You must be an authorized user. </label>
+          </div>
+        </div>
+      </li
+    </ul>
+    <ul class="form-section page-section">        
+      <li class="form-line" data-type="control_text">
+        <div class="form-input-wide">
+          <div class="form-buttons-wrapper">
+           <a href="index.php">
+              <button type="button" class="form-submit-button" >
+                Login
+              </button>
+            </a>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+ </form>        
+<?php endif; ?>
  </body>
 </html>
