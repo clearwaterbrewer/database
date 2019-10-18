9fc240c39996c3296b1bb73838e6195288bf1ba7
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-17 20:17:56 -0400

    Create CDCAjax2.php

---
+++ CDCAjax2.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <style>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+  }
+
+  th {text-align: left;}
+  </style>
+ </head>
+ <body>
+ 
+  <?php
+  $q = intval($_GET['q']);
+
+ 
+$con = mysqli_connect('localhost','yellowje_DBadmin','qwer1234','yellowje_CDCtestDB');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"yellowje_CDCtestDB");
+$sql="SELECT * FROM Containers WHERE ID = '".$q."'";
+$result = mysqli_query($con,$sql);
+ 
+
+echo "<table>
+<tr>
+<th>ID</th>
+<th>ContainerName</th>
+<th>ContainerVolume</th>
+<th>Description</th>
+</tr>";
+
+
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['ID'] . "</td>";
+    echo "<td>" . $row['ContainerName'] . "</td>";
+    echo "<td>" . $row['ContainerVolume'] . "</td>";
+    echo "<td>" . $row['Description'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
+
+  <?php
+  $r = intval($_GET['r']);
+
+$a[] = "C1";
+$a[] = "C2";
+$a[] = "C3";
+$a[] = "C4";
+$a[] = "C5";
+$a[] = "C6";
+$a[] = "C7";
+$a[] = "C8";
+$a[] = "C9";
+$a[] = "C10";
+$a[] = "C11";
+$a[] = "C12";
+$a[] = "C13";
+$a[] = "C14";
+$a[] = "C15";
+$a[] = "C16";
+$a[] = "C17";
+$a[] = "C18";
+$a[] = "C19";
+
+
+// get the r parameter from URL
+$r = $_REQUEST["r"];
+
+$hint = "";
+
+// lookup all hints from array if $q is different from "" 
+if ($r !== "") {
+    $r = strtolower($r);
+    $len=strlen($r);
+    foreach($a as $name) {
+        if (stristr($r, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+// Output "no suggestion" if no hint was found or output correct values 
+echo $hint === "" ? "no suggestion" : $hint;
+?>
+
+
+ </body>
+</html>
