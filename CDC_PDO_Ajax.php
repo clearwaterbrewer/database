f788bb65bcb755bbdcdd4daa994ee26bd3a40133
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 12:01:57 -0400

    Update CDC_PDO_Ajax.php

--- CDC_PDO_Ajax.php
+++ CDC_PDO_Ajax.php
@@ -3,19 +3,6 @@ include_once 'includes/db_connect.php';
 ?>
 <!DOCTYPE html>
 <html>
- <head>
-  <style>
-  table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-  table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-  }
-  th {text-align: left;}
-  </style>
- </head>
  <body>
 
 <?php
