017f2a1334dc3f15aed4fb2a8f07bd53fcffbe42
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 14:06:27 -0400

    Update CDC_Ajax_PDO.php

--- CDC_Ajax_PDO.php
+++ CDC_Ajax_PDO.php
@@ -1,15 +1,11 @@
 <?php
-//This is how I connect to my database, 
 include_once 'includes/db_connect_PDO.php';
 ?>
-
 <!DOCTYPE html>
 <html>
  <body>
-
 <?php
 $db = new Database;
-
 $db->prepare('SELECT BatchNum, BatchName, SourceProduct, SourceIngredient FROM Batches WHERE BatchNum = ?');
 $db->execute($_POST['batchNum']);
 
