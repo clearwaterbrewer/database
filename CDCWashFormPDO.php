2a048ecfc507eca821323b5a2454dd7a383c4663
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 12:13:16 -0400

    Update CDCWashFormPDO.php

--- CDCWashFormPDO.php
+++ CDCWashFormPDO.php
@@ -1,53 +1,69 @@
-<?php include_once 'includes/db_connect.php'; ?>
+<?php include_once 'includes/db_connect.php'; 
+
+$db = new Database;
+$db->prepare("SELECT BatchNum, BatchName FROM Batches");
+$db->execute();
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
 <title>CDC Wash Run Entry</title>
-  <script>
-  function showBatch(str) {
-    if (str == "") {
-        document.getElementById("txtMessage").innerHTML = "";
-        return;
-    } 
-    else { 
-        xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtMessage").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","CDCAjax3.php?b="+str,true);
-        xmlhttp.send();
-    }
-}
-  </script>
+      <script src = includes/jquery.min.js></script>
 </head>
 <body>
-<form id="WashForm" name="washform" method="POST">
+	
+<form id="WashForm">
 <h1>CDC Wash Run Entry</h2>
 <label Logged in as: <?php echo htmlentities($_SESSION['username']); ?> </label>
 <label for="DateTimeCode">DateTimeCode</label>
- <input name="DateTimeCode" value="<?php echo date('Y-m-d H:i:s'); ?>" required><br>
-<label for="BatchNum">BatchNum</label>
-         
-	<?php
-	$sql = "SELECT BatchNum, BatchName, SourceProduct, SourceIngredient FROM Batches ORDER by BatchNum DESC";
-	$result = $mysqli->query($sql);
-	echo "<select name='BatchNum' id='BatchNum' onchange='showBatch(this.value)'>";
-	echo "<option value=''> Select BatchNum        </option>";
-	while ($row = $result->fetch_assoc()) {
-		echo "<option value='" . $row['BatchNum'] . "'>" . $row['BatchNum'] .' - '. $row['BatchName'] . "</option>";
-	}
-	echo "</select><br><br>";
-	echo "<label for='BatchName'>Batch Name</label>";
-	echo " <input name='BatchName' value='".$row['BatchName']."'><br>";
-	echo "<label for='SourceProduct'>Source Product</label>";
-	echo " <input name='SourceProduct' value='".$row['SourceProduct']."'><br>";
-	echo "<label for='SourceIngredient'>Source Ingredient</label>";
-	echo "<input name='SourceIngredient' value='".$row['SourceIngredient']."'><br>";
-	?>
+<input name="DateTimeCode" value="<?php echo date('Y-m-d H:i:s'); ?>" required><br>
+      <div>
+        <label for='batch-name'>Batch Name</label>
+        <input id="batch-name" name='BatchName' value="">
+      </div>
+      <div>
+        <label for='SourceProduct'>Source Product</label>
+        <input id="source-product" name='SourceProduct' value="">
+      </div>
+      <div>
+        <label for='SourceIngredient'>Source Ingredient</label>
+        <input id="source-ingredient" name='SourceIngredient' value="">
+      </div>
+</form>
+	
+	
+<script>
 
-       
-</form>        
+    // monitor for changes in drop-down
+    $(document).ready(function() {  // technically not necessary if script is at the bottom, but I do it out of habit
+
+      $('#choose-batch').on('change', function() {
+        retrieveIngredient( $(this).val() );
+      })
+    });
+
+    // send batchNum via ajax
+    function retrieveIngredient(batchNumber) {
+
+      $.post(
+        "CDC3.php",               // where to send data
+        {batchNum: batchNumber},  // parameters to send {varname: value}
+        function(result) {        // what to do with results
+          if(result.message=='success') {
+            populateForm(result.data);
+          } else {
+            alert ('oops, failed');
+          }
+        }
+      );
+    }
+
+    // put results into page
+    function populateForm(data) {
+      $('#batch-name').val(data.BatchName);
+      $('#source-product').val(data.SourceProduct);
+      $('#source-ingredient').val(data.SourceIngredient);
+    }
+  </script>
 </body>
 </html>
