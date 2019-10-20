abd4cd2efe12a0c7575097f53a3081e73dae9bcf
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 14:24:24 -0400

    Create CDC_WashFormPDOtest.php

---
+++ CDC_WashFormPDOtest.php
@@ -0,0 +1,80 @@
+<?php 
+define("HOST", "localhost"); 
+define("USER", "DBadmin"); 
+define("PASSWORD", "qwer1234"); 
+define("DATABASE", "CDCtest");
+
+try{
+    $dbh = new pdo( 'mysql:host='. HOST .';dbname='. DATABASE .', USER, PASSWORD,
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    die(json_encode(array('outcome' => true)));
+}
+catch(PDOException $ex){
+    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+}
+
+
+
+$db = new pdo( 'mysql:host='. HOST .';dbname='. DATABASE .', USER, PASSWORD);
+$db->prepare("SELECT BatchNum, BatchName FROM Batches");
+$db->execute();
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>CDC Wash Run Entry</title>
+      <script src = includes/jquery.min.js></script>
+</head>
+<body>
+	
+<form id="WashForm">
+<h1>CDC Wash Run Entry</h2>
+<label Logged in as: <?php echo htmlentities($_SESSION['username']); ?> </label>
+<label for="DateTimeCode">DateTimeCode</label>
+<input name="DateTimeCode" value="<?php echo date('Y-m-d H:i:s'); ?>" required><br>
+      <div>
+        <label for='batch-name'>Batch Name</label>
+        <input id="batch-name" name='BatchName' value="">
+      </div>
+      <div>
+        <label for='SourceProduct'>Source Product</label>
+        <input id="source-product" name='SourceProduct' value="">
+      </div>
+      <div>
+        <label for='SourceIngredient'>Source Ingredient</label>
+        <input id="source-ingredient" name='SourceIngredient' value="">
+      </div>
+</form>
+	
+	
+<script>
+    // monitor for changes in drop-down
+    $(document).ready(function() {  // technically not necessary if script is at the bottom, but I do it out of habit
+      $('#choose-batch').on('change', function() {
+        retrieveIngredient( $(this).val() );
+      })
+    });
+    // send batchNum via ajax
+    function retrieveIngredient(batchNumber) {
+      $.post(
+        "CDC_Ajax_PDO.php",       // where to send data
+        {batchNum: batchNumber},  // parameters to send {varname: value}
+        function(result) {        // what to do with results
+          if(result.message=='success') {
+            populateForm(result.data);
+          } else {
+            alert ('oops, failed');
+          }
+        }
+      );
+    }
+    // put results into page
+    function populateForm(data) {
+      $('#batch-name').val(data.BatchName);
+      $('#source-product').val(data.SourceProduct);
+      $('#source-ingredient').val(data.SourceIngredient);
+    }
+  </script>
+</body>
+</html>
