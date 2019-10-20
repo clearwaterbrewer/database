adae5d885dc9f1824ac388ad0655ad85a10ba8e2
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 16:31:32 -0400

    Update db_connect_PDO.php

--- includes/db_connect_PDO.php
+++ includes/db_connect_PDO.php
@@ -1,6 +1,12 @@
 <?php
+
+$servername = "localhost";
+$username = "DBadmin";
+$password = "qwer1234"; 
+$dbname = "CDCtest";
+
 try{
-    $dbh = new PDO( 'mysql:host=127.0.0.1;dbname=CDCtest','DBadmin','qwer1234');
+    $dbh = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password");
                     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     die(json_encode(array('outcome' => true)));
 }
@@ -9,4 +15,6 @@ catch(PDOException $ex){
 }
 $dbh = null;
 ?>
+
+
 
