e88c1f96858abc1bf7a26eab1fe984c3071b171c
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 12:37:04 -0400

    Update CDC_PDO_Ajax.php

--- CDC_PDO_Ajax.php
+++ CDC_PDO_Ajax.php
@@ -6,29 +6,6 @@ include_once 'includes/db_connect.php';
  <body>
 
 <?php
-$b = intval($_GET['b']);
-$sql="SELECT * FROM Batches WHERE BatchNum = '".$b."'";
-$result = $mysqli->query($sql);
-echo "<table>
-<tr>
-<th>BatchNum</th>
-<th>BatchName</th>
-<th>SourceProduct</th>
-<th>SourceIngredient</th>
-</tr>";
-while ($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['BatchNum'] . "</td>";
-    echo "<td>" . $row['BatchName'] . "</td>";
-    echo "<td>" . $row['SourceProduct'] . "</td>";
-    echo "<td>" . $row['SourceIngredient'] . "</td>";
-    echo "</tr>";
-}
-echo "</table>";
-?>
-  
-  
-<?php
 $db = new Database;
 
 $db->prepare('SELECT BatchNum, BatchName, SourceProduct, SourceIngredient FROM Batches WHERE BatchNum = ?');
