dc345a14cc788a68aab41e05c0400578cc332c79
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 18:32:02 -0400

    Update db_connect_PDO.php

--- includes/db_connect_PDO.php
+++ includes/db_connect_PDO.php
@@ -1,6 +1,5 @@
 <?php
 require_once('psl-configPDO.php');
-
 try{
     $dbh = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
                     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -8,7 +7,7 @@ try{
 }
 catch(PDOException $ex){
     die(json_encode(array('outcome' => false, 
-                          'message' => 'Unable to connect to '.$servername.':'.$dbname.' with '.$username.':'.$password)));
+                          'message' => 'Unable to connect to '.$servername.':'.$dbname.' with '.$username)));
 }
 $dbh = null;
 ?>
