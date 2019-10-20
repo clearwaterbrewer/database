057ac26b30cdb54a7d9f4bed3c4442953f278012
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 18:00:29 -0400

    Update db_connect_PDO.php

--- includes/db_connect_PDO.php
+++ includes/db_connect_PDO.php
@@ -1,18 +1,11 @@
 <?php
-
-$strServername = "localhost";
-$strDbname = "CDCtest";
-$strUsername = "DBadmin";
-$strPassword = "qwer1234"; 
-
 try{
-    $dbh = new PDO('mysql:host=$strServername;dbname=$strDbname",$strUsername,$strPassword');
+    $dbh = new PDO('mysql:host=localhost;dbname=CDCtest','DBadmin','qwer1234');
                     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     die(json_encode(array('outcome' => true)));
 }
 catch(PDOException $ex){
-    die(json_encode(array('outcome' => false, 
-                          'message' => 'Unable to connect to '.$strServername.':'.$strDbname.' with '.$strUsername.':'.$strPassword)));
+    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
 }
 $dbh = null;
 ?>
