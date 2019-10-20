7a5e343ade716214248af83bb975c146647645d8
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-20 17:46:32 -0400

    Update db_connect_PDO.php

--- includes/db_connect_PDO.php
+++ includes/db_connect_PDO.php
@@ -12,7 +12,7 @@ try{
 }
 catch(PDOException $ex){
     die(json_encode(array('outcome' => false, 
-                          'message' => 'Unable to connect to '.$strServername.':'.$strDbname.' with '.$strUsername.':'.$strPassword)));
+                          'message' => 'Unable to connect to '.$strServername.':'.$strDbname.' with '.$strUsername.':'.$strPassword.'/n')));
 }
 $dbh = null;
 ?>
