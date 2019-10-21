e8ead60cae55c0f29e028e2a83b6fe0c87a511ab
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-21 12:56:40 -0400

    Update psl-configPDO.php

--- includes/psl-configPDO.php
+++ includes/psl-configPDO.php
@@ -4,4 +4,8 @@ $username = "DBadmin";
 $password = "qwer1234"; 
 $dbname = "CDCtest";
 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+define("SECURE", FALSE);    // For development purposes only!!!!
+
 ?>
