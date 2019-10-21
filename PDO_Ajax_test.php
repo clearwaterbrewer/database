3c3853ab6c4c48edffbcba57b50098afa00af34e
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-21 09:29:47 -0400

    Update PDO_Ajax_test.php

--- PDO_Ajax_test.php
+++ PDO_Ajax_test.php
@@ -2,7 +2,7 @@
 require_once('includes/psl-configPDO.php');
 $db = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
 $BatchNum = 4;
-$sth = $dbh->prepare('SELECT BatchName
+$sth = $db->prepare('SELECT BatchName
     FROM Batches
     WHERE Batchnum = :BatchNum');
 $sth->bindParam(':BatchNum', $BatchNum, PDO::PARAM_INT);
