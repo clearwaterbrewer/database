c7296c594c1f78daba3f6bae120a1042328baf58
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-21 07:31:28 -0400

    Update CDC_Ajax_PDO.php

--- CDC_Ajax_PDO.php
+++ CDC_Ajax_PDO.php
@@ -2,7 +2,7 @@
 require_once('includes/psl-configPDO.php');
 
 $db = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
-$db->prepare('SELECT BatchNum, BatchName, SourceProduct, SourceIngredient FROM Batches WHERE BatchNum = ?');
+$db->prepare('SELECT BatchName, SourceProduct, SourceIngredient FROM Batches WHERE BatchNum = ?');
 $db->execute($_POST['BatchNum']);
 $data = new stdClass;
 $status = 'failed';
