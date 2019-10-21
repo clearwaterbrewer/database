20cf4103c176a5b78230a147a2e07f36248d1a25
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-21 14:12:02 -0400

    Update WashRunInsert.php

--- includes/WashRunInsert.php
+++ includes/WashRunInsert.php
@@ -5,7 +5,7 @@ require_once('includes/psl-configPDO.php');
 $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
 $sql = "INSERT INTO WashRuns (DateTimeCode, BatchNum) VALUES (?,?)";
 if($insertWashRun = $pdo->prepare($sql)){
-    $insertWashRun->bind_param(':$DateTimeCode', $_POST['DateTimeCode'], PDO::PARAM_STR);
+    $insertWashRun->bindParam(':$DateTimeCode', $_POST['DateTimeCode'], PDO::PARAM_STR);
 
     $insertWashRun->execute();
         
@@ -16,3 +16,4 @@ else{
     }
  
 ?>
+
