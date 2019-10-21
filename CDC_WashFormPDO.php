5fe1094fb48bb604e8ab6d66026b838ff0c338ee
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-21 11:03:55 -0400

    Update CDC_WashFormPDO.php

--- CDC_WashFormPDO.php
+++ CDC_WashFormPDO.php
@@ -72,6 +72,132 @@ $db->execute();
           <input type="text" class="form-control" size="20" id="SourceIngredient" name="SourceIngredient" value="" required>
         </div>
     </li>
+        <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="SourceAmount">Source Amount</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="SourceAmount" name="SourceAmount" value="">
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="SourceContainer">Source Container</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="SourceContainer" name="SourceContainer" value="">
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="AlcByVol">AlcByVol</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="AlcByVol" name="AlcByVol" value="">
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="GallonsDistilled">Gallons Distilled</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="GallonsDistilled" name="GallonsDistilled" value="">
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="GallonsRemaining">Gallons Remaining</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="GallonsRemaining" name="GallonsRemaining" value="">
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="StartColl">Collection Start Time</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="StartColl" name="StartColl" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="StopColl">Collection Stop Time</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="StopColl" name="StopColl" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="WGCollected">WG Collected</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="WGCollected" name="WGCollected" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="ProofCollected">Proof Collected</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="ProofCollected" name="ProofCollected" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="PGCollected">PG Collected</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="PGCollected" name="PGCollected" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="PGEfficiency">PG Efficiency</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="PGEfficiency" name="PGEfficiency" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="Temp_C">Temp_C</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="Temp_C" name="Temp_C" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="Distilled_pH">Distilled pH</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="Distilled_pH" name="Distilled_pH" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="StartProof">Start Proof</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="StartProof" name="StartProof" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="StopProof">Stop Proof</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="StopProof" name="StopProof" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="DestinationProduct">Destination Product</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="DestinationProduct" name="DestinationProduct" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="DestinationContainer">Destination Container</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="DestinationContainer" name="DestinationContainer" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="WG_existing">WG existing</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="WG_existing" name="WG_existing" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="WG_resulting">WG Resulting</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="WG_resulting" name="WG_resulting" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="PG_existing">PG Existing</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="PG_existing" name="PG_existing" value="" >
+        </div>
+    </li>
+    <li class="form-line" data-type="control_textbox">
+        <label class="form-label form-label-top form-label-auto" for="PG_resulting">PG Resulting</label>
+        <div class="form-input-wide">
+          <input type="text" class="form-control" size="20" id="PG_resulting" name="PG_resulting" value="" >
+        </div>
+    </li>
   </ul>
     <ul class="form-section page-section" >
     <li class="form-line" data-type="control_text" >
