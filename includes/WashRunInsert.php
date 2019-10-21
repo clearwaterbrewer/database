309f6c32cf3e59f8827e6da0b4e9117519e42a23
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-21 15:08:10 -0400

    Update WashRunInsert.php

--- includes/WashRunInsert.php
+++ includes/WashRunInsert.php
@@ -2,22 +2,23 @@
 include_once('db_connect.php');
 require_once('includes/psl-configPDO.php');
 
-$pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
-$sql = "INSERT INTO WashRuns (DateTimeCode, BatchNum, WashName, SourceAmount) VALUES (:DateTimeCode, :BatchNum, :WashName, :SourceAmount)";
-if($insertWashRun = $pdo->prepare($sql)){
-
-    $form = $_POST;
-    $DateTimeCode = $form[ 'DateTimeCode' ];                    
-    $BatchNum= $form[ 'BatchNum' ];                   
-    $WashName= $form[ 'WashName' ];                 
-    $SourceAmount= $form[ 'SourceAmount' ];                 
-
-    $result = $insertWashRun->execute(array(':DateTimeCode'=>$DateTimeCode, ':BatchNum'=>$BatchNum, ':WashName'=>$WashName, ':SourceAmount'=>$SourceAmount));
-        
-    header('Location: ../CDC_WashRuns.php');
-    } 
-else{
-    echo "ERROR: Could not prepare query: $sql. " . $pdo->error;
-    }
- 
+if(!empty($_POST)){
+  $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
+  $sql = "INSERT INTO WashRuns (DateTimeCode, BatchNum, WashName, SourceAmount) VALUES (:DateTimeCode, :BatchNum, :WashName, :SourceAmount)";
+  if($insertWashRun = $pdo->prepare($sql)){
+  
+      $DateTimeCode = $_POST[ 'DateTimeCode' ];                    
+      $BatchNum= $_POST[ 'BatchNum' ];                   
+      $WashName= $_POST[ 'WashName' ];                 
+      $SourceAmount= $_POST[ 'SourceAmount' ];                 
+  
+      $result = $insertWashRun->execute(array(':DateTimeCode'=>$DateTimeCode, ':BatchNum'=>$BatchNum, ':WashName'=>$WashName, ':SourceAmount'=>$SourceAmount));
+          
+      header('Location: ../CDC_WashRuns.php');
+      } 
+  else{
+      echo "ERROR: Could not prepare query: $sql. " . $pdo->error;
+      }
+exit;
+} 
 ?>
