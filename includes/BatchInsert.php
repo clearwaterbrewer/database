43268fd50426e7bedacdb00acc15c5d5de2b78f6
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-22 12:47:28 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -1,12 +1,12 @@
 <?php
-include_once 'db_connect.php';
-include_once 'psl-config.php';
+require_once('psl-configPDO.php');
+if(!empty($_POST)){
+  $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
+  $sql = "INSERT INTO RemovedFromBond ( BatchName,  ClassType,  SourceProduct,  SourceIngredient,  BottleProof,  BarrelProof,  PreviousBatch,  UPC,  CurrentBatch) 
+                               VALUES (:BatchName, :ClassType, :SourceProduct, :SourceIngredient, :BottleProof, :BarrelProof, :PreviousBatch, :UPC, :CurrentBatch)";
 
 
-$sql = "INSERT INTO Batches (BatchName, ClassType, SourceProduct, SourceIngredient, BottleProof, BarrelProof, PreviousBatch, UPC, CurrentBatch)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-if($stmt = $mysqli->prepare($sql)){
-    $stmt->bind_param("ssss", $BatchName, $ClassType, $SourceProduct, $SourceIngredient);
+if($insertBatch = $pdo->prepare($sql)){
     $BatchName = $_POST['BatchName'];
     $ClassType = $_POST['ClassType'];
     $SourceProduct = $_POST['SourceProduct'];
@@ -16,12 +16,18 @@ if($stmt = $mysqli->prepare($sql)){
     $PreviousBatch = $_POST['PreviousBatch'];
     $UPC = $_POST['UPC'];
     $CurrentBatch = '1';
-    $stmt->execute();
+    
+    $result = $insertBatch->execute(array(':BatchName'=>BatchName, ':ClassType'=>ClassType, ':SourceProduct'=>SourceProduct, ':SourceIngredient'=>SourceIngredient, ':BottleProof'=>BottleProof, ':BarrelProof'=>BarrelProof, ':PreviousBatch'=>PreviousBatch, ':UPC'=>UPC, ':CurrentBatch'=>CurrentBatch));
         
     header('Location: ../CDC_Batches.php');
+    $insertBatch = null;
+
     } 
 else{
     echo "ERROR: Could not prepare query: $sql. " . $mysqli->error;
     }
+exit;
+} 
+echo "$_POST Empty";
  
 ?>
