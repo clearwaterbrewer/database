4c82a3cb7d9049c25ea8940fb7d6d20eba0cde7f
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-22 11:30:50 -0400

    Update psl-configPDO.php

--- includes/psl-configPDO.php
+++ includes/psl-configPDO.php
@@ -8,7 +8,8 @@ $CAN_REGISTER = "any";
 $DEFAULT_ROLE = "member";
 $SECURE = FALSE;    // For development purposes only!!!!
 
-$dbh = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
+$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//$dbh = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
