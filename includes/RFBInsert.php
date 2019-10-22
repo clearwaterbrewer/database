ce1ab5c6738019ac5798841a2040a16b5a4e9bc3
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-21 21:31:36 -0400

    Update RFBInsert.php

--- includes/RFBInsert.php
+++ includes/RFBInsert.php
@@ -1,18 +1,21 @@
 <?php
-include_once 'db_connect.php';
-include_once 'psl-config.php';
-$sql = "INSERT INTO RemovedFromBond (BatchNum, BottlesRemoved, BottlesRemaining, CaseNumbers, Destination, InvoiceNumber) 
-        VALUES (?, ?, ?, ?, ?, ?)";
-if($stmt = $mysqli->prepare($sql)){
-    $stmt->bind_param("ssss", $BatchNum, $BottlesRemoved, $BottlesRemaining, $CaseNumbers, $Destination, $InvoiceNumber);
-    $BatchNum = $_POST['BatchNum'];
-    $BottlesRemoved = $_POST['BottlesRemoved'];
-    $BottlesRemaining = $_POST['BottlesRemaining'];
-    $CaseNumbers = $_POST['CaseNumbers'];
-    $Destination = $_POST['Destination'];
-    $InvoiceNumber = $_POST['InvoiceNumber'];
-    $stmt->execute();
-        
+require_once('psl-configPDO.php');
+if(!empty($_POST)){
+  $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
+  $sql = "INSERT INTO RemovedFromBond (DateTimeCode, BatchNum, BottlesRemoved, BottlesRemaining, CaseNumbers, Destination, InvoiceNumber) 
+                          VALUES (:DateTimeCode, :BatchNum, :BottlesRemoved, :BottlesRemaining, :CaseNumbers, :Destination, :InvoiceNumber)";
+  if($insertWashRun = $pdo->prepare($sql)){
+  
+      $DateTimeCode = $_POST[ 'DateTimeCode' ];                    
+      $BatchNum= $_POST[ 'BatchNum' ];                   
+      $BottlesRemoved= $_POST[ 'BottlesRemoved' ];                 
+      $BottlesRemaining= $_POST[ 'BottlesRemaining' ];                 
+      $CaseNumbers= $_POST[ 'CaseNumbers' ];                 
+      $Destination= $_POST[ 'Destination' ];                 
+      $InvoiceNumber= $_POST[ 'InvoiceNumber' ];                 
+  
+      $result = $insertWashRun->execute(array(':DateTimeCode'=>$DateTimeCode, ':BatchNum'=>$BatchNum, ':BottlesRemoved'=>$BottlesRemoved, ':BottlesRemaining'=>$BottlesRemaining, ':CaseNumbers'=>$CaseNumbers, ':Destination'=>$Destination, ':InvoiceNumber'=>$InvoiceNumber));
+          
     header('Location: ../CDC_RemoveFromBond.php');
     } 
 else{
@@ -20,3 +23,5 @@ else{
     }
  
 ?>
+
+
