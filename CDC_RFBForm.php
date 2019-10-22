e6e1c21792198a1d71a07e4a46f4d518fe8df801
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-22 12:25:14 -0400

    Update CDC_RFBForm.php

--- CDC_RFBForm.php
+++ CDC_RFBForm.php
@@ -1,7 +1,6 @@
 <?php
-require_once('includes/psl-configPDO.php');
-include_once('includes/db_connect.php');
-require_once('includes/functions.php');
+require_once('includes/psl-configPDO.php'); //db info
+require_once('includes/functions.php'); //security functions
 $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
 $getBatchNum = $pdo->prepare("SELECT BatchNum, BatchName FROM Batches WHERE CurrentBatch=1 ORDER by BatchNum DESC");
 $getBatchNum->execute();
