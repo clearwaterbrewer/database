cc28eef994c2d6da30f93e1d2da745f70c3dc5f3
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-22 11:32:37 -0400

    Update CDC_WashRunForm.php

--- CDC_WashRunForm.php
+++ CDC_WashRunForm.php
@@ -1,5 +1,6 @@
 <?php
 require_once('includes/psl-configPDO.php');
+include_once('includes/db_connect.php');
 require_once('includes/functions.php');
 $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
 $getBatchNum = $pdo->prepare("SELECT BatchNum, BatchName FROM Batches ORDER by BatchNum DESC");
