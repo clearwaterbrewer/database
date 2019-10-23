2ee657819e1c8ab34df6396c72c13f728e5fc2a2
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-23 18:51:12 -0400

    Update WashRunInsert.php

--- includes/WashRunInsert.php
+++ includes/WashRunInsert.php
@@ -3,7 +3,7 @@ require_once('psl-configPDO.php');
 
 if(!empty($_POST)){
   $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
-  $sql = "INSERT INTO WashRuns (DateTimeCode, BatchNum, WashName, SourceAmount, AlcByVol) VALUES (:DateTimeCode, :BatchNum, :WashName, :SourceAmount, :AlcByVol)";
+  $sql = "INSERT INTO WashRuns (DateTimeCode, BatchNum, WashName, SourceAmount, AlcByVol, GallonsDistilled, GallonsRemaining) VALUES (:DateTimeCode, :BatchNum, :WashName, :SourceAmount, :AlcByVol, :GallonsDistilled, :GallonsRemaining)";
   if($insertWashRun = $pdo->prepare($sql)){
 
       $DateTimeCode = $_POST[ 'DateTimeCode' ];
@@ -11,8 +11,10 @@ if(!empty($_POST)){
       $WashName= $_POST[ 'WashName' ];
       $SourceAmount= $_POST[ 'SourceAmount' ];
       $AlcByVol= $_POST[ 'AlcByVol' ];                 
+      $GallonsRemaining= $_POST[ 'GallonsRemaining' ];                 
+      $StartColl= $_POST[ 'StartColl' ];                 
 
-      $result = $insertWashRun->execute(array(':DateTimeCode'=>$DateTimeCode, ':BatchNum'=>$BatchNum, ':WashName'=>$WashName, ':SourceAmount'=>$SourceAmount, ':AlcByVol'=>$AlcByVol));
+      $result = $insertWashRun->execute(array(':DateTimeCode'=>$DateTimeCode, ':BatchNum'=>$BatchNum, ':WashName'=>$WashName, ':SourceAmount'=>$SourceAmount, ':AlcByVol'=>$AlcByVol, ':GallonsDistilled'=>$GallonsDistilled, ':GallonsRemaining'=>$GallonsRemaining));
 
       header('Location: ../CDC_WashRuns.php');
       $insertWashRun = null;
