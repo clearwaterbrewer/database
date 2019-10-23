928b328352e755dbcccf5416238fb21788fee16e
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-23 16:57:56 -0400

    Update CDC_WashRunForm.php

--- CDC_WashRunForm.php
+++ CDC_WashRunForm.php
@@ -1,3 +1,4 @@
+pi@raspberrypi:/var/www/html/database $ more CDC_WashRunForm.php
 <?php
 require_once('includes/psl-configPDO.php'); //db info
 require_once('includes/functions.php'); //security functions
@@ -27,7 +28,8 @@ sec_session_start();
 <body>
 
 <?php if (login_check($mysqli) == true) : ?>
-	
+
+
 <form class="jotform-form" accept-charset="utf-8" id="WashForm" name="washform" action="includes/WashRunInsert.php" method="POST">
 <div class="form-all">
   <ul class="form-section page-section">
@@ -44,9 +46,9 @@ sec_session_start();
        <label for="DateTimeCode">DateTimeCode</label>
           <input type="text" class="form-control" name="DateTimeCode" value="<?php echo date('Y-m-d H:i:s'); ?>" required>
         <label for="choose-batch">Choose a Batch</label>
-	  <select class='form-dropdown' id='choose-batch'>
+          <select class='form-dropdown' id='choose-batch'>
           <?php while($row = $getBatchNum->fetchObject()): ?>
-	  <option value="<?= $row->BatchNum ?>" ><?= $row->BatchNum." - ".$row->BatchName ?></option>
+          <option value="<?= $row->BatchNum ?>" ><?= $row->BatchNum." - ".$row->BatchName ?></option>
           <?php endwhile; ?>
           </select>
         <label for="BatchName">Batch Name</label>
@@ -66,81 +68,81 @@ sec_session_start();
         <label for="WashName">Wash Name</label>
           <input type="text" class="form-control" id="WashName" name="WashName" value="">
         <label for="SourceAmount">Source Amount</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,5}$" size="5" class="form-control" id="SourceAmount" name="SourceAmount" value="">
+          <input type="text" class="form-control" id="SourceAmount" name="SourceAmount" value="">
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="SourceContainer">Source Container</label>
-	  <select class='form-dropdown' id='SourceContainer' name="SourceContainer" >
+          <select class='form-dropdown' id='SourceContainer' name="SourceContainer" >
           <?php while($row = $getSourceContainer->fetchObject()): ?>
-		<option value="<?= $row->id ?>" ><?= $row->id." - ".$row->ContainerName ?></option>
+                <option value="<?= $row->id ?>" ><?= $row->id." - ".$row->ContainerName ?></option>
           <?php endwhile; ?>
           </select>
         <label for="AlcByVol">AlcByVol</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,2}(\.\d{0,1})?$" size="4" class="form-control" id="AlcByVol" name="AlcByVol" value="">
+          <input type="text" class="form-control" id="AlcByVol" name="AlcByVol" value="">
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="GallonsDistilled">Gallons Distilled</label>
-          <input type="text" inputmode="numeric" pattern="^\d{0,4}(\.\d{0,1})?$" size="6" class="form-control" id="GallonsDistilled" name="GallonsDistilled" value="">
+          <input type="text" class="form-control" id="GallonsDistilled" name="GallonsDistilled" value="">
         <label for="GallonsRemaining">Gallons Remaining</label>
-          <input type="text" inputmode="numeric" pattern="^\d{0,4}(\.\d{0,1})?$" size="6" class="form-control" id="GallonsRemaining" name="GallonsRemaining" value="">
+          <input type="text" class="form-control" id="GallonsRemaining" name="GallonsRemaining" value="">
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
          <label for="StartColl">Collection Start Time</label>
-          <input type="text" inputmode="numeric" pattern="(\d{2}:\d{2})" size="5" class="form-control" id="StartColl" name="StartColl" value="" >
+          <input type="text" class="form-control" id="StartColl" name="StartColl" value="" >
          <label for="StopColl">Collection Stop Time</label>
-          <input type="text" inputmode="numeric" pattern="(\d{2}:\d{2})" size="5" class="form-control" id="StopColl" name="StopColl" value="" >
+          <input type="text" class="form-control" id="StopColl" name="StopColl" value="" >
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="WGCollected">WG Collected</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,3}(\.\d{0,1})?$" size="5" class="form-control" id="WGCollected" name="WGCollected" value="" >
+          <input type="text" class="form-control" id="WGCollected" name="WGCollected" value="" >
         <label for="ProofCollected">Proof Collected</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,3}(\.\d{0,1})?$" size="5" class="form-control" id="ProofCollected" name="ProofCollected" value="" >
+          <input type="text" class="form-control" id="ProofCollected" name="ProofCollected" value="" >
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="PGCollected">PG Collected</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,3}(\.\d{0,1})?$" size="5" class="form-control" id="PGCollected" name="PGCollected" value="">
+          <input type="text" class="form-control" id="PGCollected" name="PGCollected" value="" readonly>
         <label  for="PGEfficiency">PG Efficiency</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,3}(\.\d{0,1})?$" size="5" class="form-control" id="PGEfficiency" name="PGEfficiency" value="">
+          <input type="text" class="form-control" id="PGEfficiency" name="PGEfficiency" value="" readonly>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="Temp_C">Temp_C</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,3}(\.\d{0,1})?$" size="5" class="form-control" size="10" id="Temp_C" name="Temp_C" value="" >
+          <input type="text" class="form-control" size="10" id="Temp_C" name="Temp_C" value="" >
         <label for="Distilled_pH">Distilled pH</label>
-          <input type="text" inputmode="numeric" pattern="^\d(\.\d{1,2})?$" size="4" class="form-control" size="7" id="Distilled_pH" name="Distilled_pH" value="" >
+          <input type="text" class="form-control" size="7" id="Distilled_pH" name="Distilled_pH" value="" >
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="StartProof">Start Proof</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,3}(\.\d{0,1})?$" size="5" class="form-control" id="StartProof" name="StartProof" value="" >
+          <input type="text" class="form-control" id="StartProof" name="StartProof" value="" >
         <label for="StopProof">Stop Proof</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,3}(\.\d{0,1})?$" size="5" class="form-control" id="StopProof" name="StopProof" value="" >
+          <input type="text" class="form-control" id="StopProof" name="StopProof" value="" >
         </div>
     </li>
     <li class="form-line" data-type="control_dropdown">
         <div class="form-input-wide">
         <label for="DestinationProduct">Destination Product</label>
-		<select class='form-dropdown' id='DestinationProduct' name="DestinationProduct" >
+                <select class='form-dropdown' id='DestinationProduct' name="DestinationProduct" >
           <?php while($row = $getDestinationProduct->fetchObject()): ?>
-		<option value="<?= $row->id ?>" ><?= $row->id." - ".$row->ProductName ?></option>
+                <option value="<?= $row->id ?>" ><?= $row->id." - ".$row->ProductName ?></option>
           <?php endwhile; ?>
           </select>
         <label for="DestinationContainer">Destination Container</label>
-		<select class='form-dropdown' id='DestinationContainer' name="DestinationContainer" >
+                <select class='form-dropdown' id='DestinationContainer' name="DestinationContainer" >
           <?php while($row = $getDestinationContainer->fetchObject()): ?>
-		<option value="<?= $row->id ?>" ><?= $row->id." - ".$row->ContainerName ?></option>
+                <option value="<?= $row->id ?>" ><?= $row->id." - ".$row->ContainerName ?></option>
           <?php endwhile; ?>
           </select>
         </div>
@@ -148,17 +150,17 @@ sec_session_start();
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="WG_existing">WG existing</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,4}(\.\d{0,1})?$" size="6" class="form-control" id="WG_existing" name="WG_existing" value="" >
+          <input type="text" class="form-control" id="WG_existing" name="WG_existing" value="" >
         <label for="WG_resulting">WG Resulting</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,4}(\.\d{0,1})?$" size="6" class="form-control" id="WG_resulting" name="WG_resulting" value="" >
+          <input type="text" class="form-control" id="WG_resulting" name="WG_resulting" value="" >
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="PG_existing">PG Existing</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,4}(\.\d{0,1})?$" size="6" class="form-control" id="PG_existing" name="PG_existing" value="" >
+          <input type="text" class="form-control" id="PG_existing" name="PG_existing" value="" >
         <label for="PG_resulting">PG Resulting</label>
-          <input type="text" inputmode="numeric" pattern="^\d{1,4}(\.\d{0,1})?$" size="6" class="form-control" id="PG_resulting" name="PG_resulting" value="" >
+          <input type="text" class="form-control" id="PG_resulting" name="PG_resulting" value="" >
         </div>
     </li>
   </ul>
@@ -173,12 +175,12 @@ sec_session_start();
     </li>
   </ul>
 </div>
-</form>        
+</form>
 
   <script>
 
     // monitor for changes in drop-down
-    $(document).ready(function() {  
+    $(document).ready(function() {
 
       $('#choose-batch').on('change', function() {
         retrieveItem( $(this).val() );
@@ -186,32 +188,12 @@ sec_session_start();
     });
 
     // monitor for changes in ProofCollected
-    $(document).ready(function() {  
+    $(document).ready(function() {
 
       $('#ProofCollected').on('focusout', function() {
         calculatePG();
       })
     });
-
-	  
-    var StartColl = $("#StartColl");
-    var StopColl = $("#StopColl");
-    function formatTime(e) {
-      var r = /([a-f0-9]{2})([a-f0-9]{2})/i,
-          str = e.target.value.replace(/[^a-f0-9]/ig, "");
-      while (r.test(str)) {
-        str = str.replace(r, '$1' + ':' + '$2');
-      }
-
-      e.target.value = str.slice(0, 5);
-    };
-	  
-    // monitor for changes in StartColl and StopColl
-    StartColl.on("keyup", formatTime);
-    StopColl.on("keyup", formatTime);
-
-
-
 
     // send batchNum via ajax
     function retrieveItem(BatchNumber) {
@@ -235,7 +217,7 @@ sec_session_start();
       var PGCollected = num_format(PGCollected, 2);
       document.getElementById('PGCollected').value = PGCollected;
     }
-  
+
 
     // put results into page
     function populateForm(data) {
@@ -246,14 +228,12 @@ sec_session_start();
     function populatePG(data) {
       $('#PGCollected').val(data.PGCollected);
     }
-	  
-	  
   </script>
 
 <?php else : ?>
 <form class="jotform-form" accept-charset="utf-8">
   <div class="form-all">
-    <ul class="form-section page-section">        
+    <ul class="form-section page-section">
       <li id="cid_1" class="form-input-wide" data-type="control_head">
         <div class="form-header-group ">
           <div class="header-text httal htvam">
@@ -265,7 +245,7 @@ sec_session_start();
         </div>
       </li
     </ul>
-    <ul class="form-section page-section">        
+    <ul class="form-section page-section">
       <li class="form-line" data-type="control_text">
         <div class="form-input-wide">
           <div style="margin-left:40px" class="form-buttons-wrapper">
@@ -279,7 +259,7 @@ sec_session_start();
       </li>
     </ul>
   </div>
- </form>        
+ </form>
 <?php endif; ?>
 
 </body>
