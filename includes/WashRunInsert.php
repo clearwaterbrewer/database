b44e2e5e8e25d5406492698466f642a87d85b446
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-23 18:36:59 -0400

    Update WashRunInsert.php

--- includes/WashRunInsert.php
+++ includes/WashRunInsert.php
@@ -3,15 +3,16 @@ require_once('psl-configPDO.php');
 
 if(!empty($_POST)){
   $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
-  $sql = "INSERT INTO WashRuns (DateTimeCode, BatchNum, WashName, SourceAmount) VALUES (:DateTimeCode, :BatchNum, :WashName, :SourceAmount)";
+  $sql = "INSERT INTO WashRuns (DateTimeCode, BatchNum, WashName, SourceAmount, AlcByVol) VALUES (:DateTimeCode, :BatchNum, :WashName, :SourceAmount, :AlcByVol)";
   if($insertWashRun = $pdo->prepare($sql)){
 
       $DateTimeCode = $_POST[ 'DateTimeCode' ];
       $BatchNum= $_POST[ 'BatchNum' ];
       $WashName= $_POST[ 'WashName' ];
       $SourceAmount= $_POST[ 'SourceAmount' ];
+      $AlcByVol= $_POST[ 'AlcByVol' ];                 
 
-      $result = $insertWashRun->execute(array(':DateTimeCode'=>$DateTimeCode, ':BatchNum'=>$BatchNum, ':WashName'=>$WashName, ':SourceAmount'=>$SourceAmount));
+      $result = $insertWashRun->execute(array(':DateTimeCode'=>$DateTimeCode, ':BatchNum'=>$BatchNum, ':WashName'=>$WashName, ':SourceAmount'=>$SourceAmount, ':AlcByVol'=>$AlcByVol));
 
       header('Location: ../CDC_WashRuns.php');
       $insertWashRun = null;
