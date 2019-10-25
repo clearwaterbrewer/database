aabfa5dc6ac478a70a0c00cdd6378c9df2cc3364
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-25 10:21:14 -0400

    Update BatchInsert.php

--- includes/BatchInsert.php
+++ includes/BatchInsert.php
@@ -1,7 +1,7 @@
 <?php
 require_once('psl-configPDO.php');
 if(!empty($_POST)){
-  $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$username,$password);
+  $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$dbusern,$dbpassw);
   $sql = "INSERT INTO Batches ( BatchName,  ClassType,  SourceProduct,  SourceIngredient,  BottleProof,  BarrelProof,  PreviousBatch,  UPC) 
                        VALUES (:BatchName, :ClassType, :SourceProduct, :SourceIngredient, :BottleProof, :BarrelProof, :PreviousBatch, :UPC)";
 
