1f244f7269e2684b40bc6213b4c71b56cbfcbde1
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-10-26 01:20:48 -0400

    Update csvtest.php

--- csvtest.php
+++ csvtest.php
@@ -1,7 +1,6 @@
 <?php
  // from https://thisinterestsme.com/php-create-csv-file-mysql/
 require_once('includes/psl-configPDO.php');
-
 $fileName = './RFB.csv';
 $table = 'RemovedFromBond';
  
@@ -32,12 +31,12 @@ if(!empty($rows)){
  
  
 //Set the Content-Type and Content-Disposition headers to force the download.
-header('Content-Type: application/excel');
-header('Content-Disposition: attachment; filename="' . $fileName . '"');
  
 //Open up a file pointer
 $fp = fopen('php://output', 'w');
- 
+header('Content-Type: application/excel');
+header('Content-Disposition: attachment; filename="' . $fileName . '"');
+
 //Start off by writing the column names to the file.
 fputcsv($fp, $columnNames);
  
