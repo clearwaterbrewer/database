f06accf641716db7403a4b28a0e99519bad7111d
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-11-24 06:28:13 -0500

    Update register.inc.php

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -4,7 +4,7 @@ include_once 'psl-config.php';
 
 $error_msg = "";
 
-if (isset($_POST['username'], $_POST['email'], $_POST['p'])) {
+if (isset($_POST['username'], $_POST['email'], $_POST['firstname'], $_POST['lastname'], $_POST['initials'], $_POST['p'])) {
     // Sanitize and validate the data passed in
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
