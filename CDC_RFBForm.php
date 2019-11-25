450edeb3a702ccd37605a6366ca5b563c4a762df
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-11-25 09:35:22 -0500

    Update CDC_RFBForm.php

--- CDC_RFBForm.php
+++ CDC_RFBForm.php
@@ -88,7 +88,7 @@ sec_session_start();
         </div>
         <div class="form-input-wide">
          <label> - </label>
-	 <input type="hidden" value="<?php htmlentities($_SESSION['initials']); ?>" >
+	 <input type="hidden" id="Initials" name="Initials" value="<?php htmlentities($_SESSION['initials']); ?>" >
        </div>
     </li>
     </li>
