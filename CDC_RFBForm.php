89b65992adc5f23303df85ccf955ae74cf225936
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-11-27 15:07:05 -0500

    Update CDC_RFBForm.php

--- CDC_RFBForm.php
+++ CDC_RFBForm.php
@@ -117,7 +117,7 @@ sec_session_start();
 	    $('#CaseCount, #BottleCount').on('change', function() { 
 //		    var value1 = ('#CaseCount').val();
 //		    var value2 = ('#BottleCount').val();
-		    $('#BottlesRemaining').val('#CaseCount');
+		    $('#BottlesRemaining').val('#CaseCount.val()');
 		    });
     });
     // send batchNum via ajax
