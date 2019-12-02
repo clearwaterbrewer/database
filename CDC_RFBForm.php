ad91b4b366e820e8a7fcb707fa31797a0205e37f
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2019-12-02 12:36:31 -0500

    Update CDC_RFBForm.php

--- CDC_RFBForm.php
+++ CDC_RFBForm.php
@@ -93,6 +93,7 @@ sec_session_start();
         <div class="form-input-wide">
          <label> - </label>
 	 <input type="hidden" id="Initials" name="Initials" value="<?php echo $_SESSION['initials']; ?>" >
+	 <input type="submit" id="createPDF" name="createPDF" value="Create PDF" >
        </div>
     </li>
     </li>
@@ -103,7 +104,6 @@ sec_session_start();
             <button type="submit" class="form-submit-button" id="submit_form" name="submit" value="Submit">Submit</button>
           <a href="CDC_RemoveFromBond.php"><button type="button" class="form-submit-button">Back to RFB</button></a>
           <a href="CDChome.php"><button type="button" class="form-submit-button">Home</button></a>
-          <a href="CDC_RFBPrint.php"><button type="button" class="form-submit-button">Print(future)</button></a>
           <a href="includes/logout.php"><button type="button" class="form-submit-button">Logout</button></a>
       </div>
     </li>
