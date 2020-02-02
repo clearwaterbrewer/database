687e8ab4a4490d4f376244dfe9f8be0e77d6909e
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-02-02 15:47:01 -0500

    Update CDC_RFBForm.php

--- CDC_RFBForm.php
+++ CDC_RFBForm.php
@@ -2,7 +2,7 @@
 require_once('includes/psl-configPDO.php'); //db info
 require_once('includes/functions.php'); //security functions
 $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$dbusern,$dbpassw);
-$getBatchNum = $pdo->prepare("SELECT BatchNum, BatchName FROM Batches WHERE CurrentBatch=1 ORDER by BatchNum DESC");
+$getBatchNum = $pdo->prepare("SELECT BatchNum, BatchName FROM Batches WHERE CurrentBatch=1 AND UPC IS NOT NULL ORDER by BatchNum DESC");
 $getBatchNum->execute();
 $getDestination = $pdo->prepare("SELECT DistributorsID, DistributorsName FROM Distributors");
 $getDestination->execute();
