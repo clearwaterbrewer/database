d51aa5ac186941e292a3ca3239f804906dc73936
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-02-04 11:50:55 -0500

    Update CDC_BottleInventory.php

--- CDC_BottleInventory.php
+++ CDC_BottleInventory.php
@@ -43,7 +43,7 @@ sec_session_start();
     </li>
     <li class="form-line" data-type="control_textbox">
  	<div class="form-input-wide"">
-	  <label>Batch        </label>
+	  <label>"Batch        "</label>
 	  <label>Name         </label>
 	  <label>UPC          </label>
 	  <label>Cases      </label>
