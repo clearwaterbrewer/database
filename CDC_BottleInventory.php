81da7cdfc6f608ef2a4bbc22d8570c6d53101b04
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-02-05 14:12:50 -0500

    Update CDC_BottleInventory.php

--- CDC_BottleInventory.php
+++ CDC_BottleInventory.php
@@ -122,16 +122,10 @@ sec_session_start();
         $('#Counted4').val(Counted4);
         });
       $('#CaseCount5, #BottleCount5').on('input', function() { 
-//        var Counted5 = 0; // this works
-//        var ['Counted' + '5'] = 0; // this does not work and breaks previous functions
-//        var [Counted + '5'] = 0; // this does not work and breaks previous functions
-//        var ['Counted' + 5] = 0; // this does not work and breaks previous functions
-//        var String('Counted' + '5') = 0; // this does not work and breaks previous functions
-        this["Counted"+"5"] = 0; // this does not work and breaks previous functions
-//        this["Counted"+'5'] = "0"; // this does not work and breaks previous functions
+        this["Counted"+"5"] = 0;
 	var value1 = $('#CaseCount5').val();
         var value2 = $('#BottleCount5').val();
-        var Counted5 = (value1 * 12) + (value2 * 1);
+        this["Counted"+"5"] = (value1 * 12) + (value2 * 1);
         $('#Counted5').val(Counted5);
         });
     });
