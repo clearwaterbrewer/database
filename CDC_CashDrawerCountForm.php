106cbf48ebd9d7f9966b61cd13a6b3c8ee41f32b
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-16 15:47:35 -0500

    Update CDC_CashDrawerCountForm.php

--- CDC_CashDrawerCountForm.php
+++ CDC_CashDrawerCountForm.php
@@ -32,7 +32,7 @@ sec_session_start();
        <label for="DateTimeCode">DateTimeCode</label>
           <input type="text" class="form-control" name="DateTimeCode" value="<?php echo date('Y-m-d H:i:s'); ?>" required>
         <label for="CountOpen">Opening Count</label>
-          <input type='text' inputmode="numeric" class='form-control' id="CountOpen" name="CountOpen" value="" />
+          <input type='text' inputmode="numeric" pattern="^\d{1,8}$" size="8"class='form-control' id="CountOpen" name="CountOpen" value="" />
        </div>
     </li>
 
@@ -71,13 +71,13 @@ sec_session_start();
         <div class="form-input-wide">
         <label for="bill_5"> $5 bills</label>
           <input type="text" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_5" name="bill_5" value="" >
-        <label for="coin_5"> nickels</label>
+        <label for="coin_5"> _nickels</label>
           <input type="text" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_5" name="coin_5" value="" >
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
-        <label for="bill_1"> $1 bills</label>
+        <label for="bill_1"> $1 bills_</label>
           <input type="text" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_1" name="bill_1" value="" >
         <label for="coin_1"> pennies</label>
           <input type="text" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_1" name="coin_1" value="" >
