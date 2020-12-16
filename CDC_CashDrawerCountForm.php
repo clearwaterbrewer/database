ea83c3fd29de2ff17ff8fa4184a3f37256636d7e
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-16 17:49:45 -0500

    Update CDC_CashDrawerCountForm.php

--- CDC_CashDrawerCountForm.php
+++ CDC_CashDrawerCountForm.php
@@ -139,8 +139,8 @@ sec_session_start();
 		    var coin10 = $('#coin_10').val();
 		    var coin5 = $('#coin_5').val();
 		    var coin1 = $('#coin_1').val();
-		    var coins = (coin100 / 1);
-		    $('#coins').val(coins);
+		    var coins = (coin100 * 100) + (coin25 * 25) + (coin10 * 10) + (coin5 * 5) + (coin1 * 1);
+		    $('#coins').val(coins.toFixed(2));
 		    });
     });
    
