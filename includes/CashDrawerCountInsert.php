96d5178864069fd67be4039235f1f46bfee996c0
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-17 11:43:28 -0500

    Update CashDrawerCountInsert.php

--- includes/CashDrawerCountInsert.php
+++ includes/CashDrawerCountInsert.php
@@ -32,7 +32,7 @@ if(!empty($_POST)){
 (DateTimeCode, Initials, CountOpen, bill_100, bill_50, bill_20, bill_10, bill_5, bill_1, coin_100, coin_25, coin_10, coin_5, coin_1, CountClose, CountDelta, POS_Amount, POS_Tip, POS_Due)<br>
 VALUES 
 (";
-	  echo $_POST["DateTimeCode"] . ", " . $_POST["Initials"] . ", " . $_POST["CountOpen"] . ", " . $_POST["bill_100"] . ", " . $_POST["bill_50"] . ", " . $_POST["bill_20"] . ", " . $_POST["bill_10"] . ", " . $_POST["bill_5"] . ", " . $_POST["bill_1"] . ", " . $_POST["coin_100"] . ", " . $_POST["coin_25"] . ", " . $_POST["coin_10"] . ", " . $_POST["coin_5"] . ", " . $_POST["coin_1"] . ", " . $_POST["CountClose"] . ", " . $_POST["CountDelta"] . ", " . $_POST["POS_Amount"] . ", " . $_POST["POS_Tip"] . ", " . $_POST["POS_Due"];
+	  echo $_POST["DateTimeCode"] . ", " . $_POST["Initials"] . ", " . $_POST["CountOpen"] . ", " . $_POST["bill_100"] . ", " . $_POST["bill_50"] . ", " . $_POST["bill_20"] . ", " . $_POST["bill_10"] . ", " . $_POST["bill_5"] . ", " . $_POST["bill_1"] . ", " . $_POST["coin_100"] . ", " . $_POST["coin_25"] . ", " . $_POST["coin_10"] . ", " . $_POST["coin_5"] . ", " . $_POST["coin_1"] . ", " . $_POST["CountClose"] . ", " . $_POST["CountDelta"] . ", " . $_POST["POS_Amount"] . ", " . $_POST["POS_Tip"] . ", " . $_POST["POS_Due"] .")";
 
       } 
   else{
