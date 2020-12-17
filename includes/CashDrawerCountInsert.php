87c7555bda4726a0fd1bf4289fc69e5e1b0962ab
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-16 23:02:56 -0500

    Update CashDrawerCountInsert.php

--- includes/CashDrawerCountInsert.php
+++ includes/CashDrawerCountInsert.php
@@ -3,7 +3,7 @@ require_once('psl-configPDO.php');
 if(!empty($_POST)){
   $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname,$dbusern,$dbpassw);
   $sql = "INSERT INTO DrawerCounts (DateTimeCode, Initials, CountOpen, bill_100, bill_50, bill_20, bill_10, bill_5, bill_1, coin_100, coin_25, coin_10, coin_5, coin_1, CountClose, CountDelta, POS_Amount, POS_Tip, POS_Due)";
-  if($insertRFB = $pdo->prepare($sql)){
+  if($insertDrawerCount = $pdo->prepare($sql)){
       $DateTimeCode = $_POST[ 'DateTimeCode' ];                    
       $Initials = $_POST['Initials' ];                 
       $CountOpen = $_POST[ 'CountOpen' ];                   
@@ -24,10 +24,11 @@ if(!empty($_POST)){
       $POS_Tip = $_POST[ 'POS_Tip' ];                 
       $POS_Due = $_POST[ 'POS_Due' ];                 
   
-      $result = $insertRFB->execute(array(':DateTimeCode'=>$DateTimeCode, ':Initials'=>$Initials, ':CountOpen'=>$CountOpen, ':bill_100'=>$bill_100, ':bill_50'=>$bill_50, ':bill_20'=>$bill_20, ':bill_10'=>$bill_10, ':bill_5'=>$bill_5, ':bill_1'=>$bill_1, ':coin_100'=>$coin_100, ':coin_25'=>$coin_25, ':coin_10'=>$coin_10, ':coin_5'=>$coin_5, ':coin_1'=>$coin_1, ':CountClose'=>$CountClose, ':CountDelta'=>$CountDelta, ':POS_Amount'=>$POS_Amount, ':POS_Tip'=>$POS_Tip, ':POS_Due'=>$POS_Due ));
+      $result = $DrawerCount->execute(array(':DateTimeCode'=>$DateTimeCode, ':Initials'=>$Initials, ':CountOpen'=>$CountOpen, ':bill_100'=>$bill_100, ':bill_50'=>$bill_50, ':bill_20'=>$bill_20, ':bill_10'=>$bill_10, ':bill_5'=>$bill_5, ':bill_1'=>$bill_1, ':coin_100'=>$coin_100, ':coin_25'=>$coin_25, ':coin_10'=>$coin_10, ':coin_5'=>$coin_5, ':coin_1'=>$coin_1, ':CountClose'=>$CountClose, ':CountDelta'=>$CountDelta, ':POS_Amount'=>$POS_Amount, ':POS_Tip'=>$POS_Tip, ':POS_Due'=>$POS_Due ));
 
 //      header('Location: ../CDC_CashDrawerCountForm.php');
-      $insertRFB = null;
+      $insertDrawerCount = null;
+      echo "It seemed to work.";
       } 
   else{
       echo "ERROR: Could not prepare query: $sql. " . $pdo->error;
