f1a0510b8d287274b7a4063b4a5146b0ecd7c7bb
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-17 09:13:27 -0500

    Update CDC_CashDrawerTestDisplayInsert.php

--- includes/CDC_CashDrawerTestDisplayInsert.php
+++ includes/CDC_CashDrawerTestDisplayInsert.php
@@ -1,25 +1,25 @@
 <html>
 <body>
 
-Date <?php echo $_GET["DateTimeCode"]; ?><br>
-Initials <?php echo $_GET["Initials"]; ?><br>
-CountOpen <?php echo $_GET["CountOpen"]; ?><br>
-bill_100 <?php echo $_GET["bill_100"]; ?><br>
- bill_50 <?php echo $_GET["bill_50"]; ?><br>
- bill_20 <?php echo $_GET["bill_20"]; ?><br>
- bill_10 <?php echo $_GET["bill_10"]; ?><br>
-  bill_5 <?php echo $_GET["bill_5"]; ?><br>
-  bill_1 <?php echo $_GET["bill_1"]; ?><br>
-coin_100 <?php echo $_GET["coin_100"]; ?><br>
- coin_25 <?php echo $_GET["coin_25"]; ?><br>
- coin_10 <?php echo $_GET["coin_10"]; ?><br>
-  coin_5 <?php echo $_GET["coin_5"]; ?><br>
-  coin_1 <?php echo $_GET["coin_1"]; ?><br>
-CountClose <?php echo $_GET["CountClose"]; ?><br>
-CountDelta <?php echo $_GET["CountDelta"]; ?><br>
-POS_Amount <?php echo $_GET["POS_Amount"]; ?><br>
-   POS_Tip <?php echo $_GET["POS_Tip"]; ?><br>
-   POS_Due <?php echo $_GET["POS_Due"]; ?><br>
+Date <?php echo $_POST["DateTimeCode"]; ?><br>
+Initials <?php echo $_POST["Initials"]; ?><br>
+CountOpen <?php echo $_POST["CountOpen"]; ?><br>
+bill_100 <?php echo $_POST["bill_100"]; ?><br>
+ bill_50 <?php echo $_POST["bill_50"]; ?><br>
+ bill_20 <?php echo $_POST["bill_20"]; ?><br>
+ bill_10 <?php echo $_POST["bill_10"]; ?><br>
+  bill_5 <?php echo $_POST["bill_5"]; ?><br>
+  bill_1 <?php echo $_POST["bill_1"]; ?><br>
+coin_100 <?php echo $_POST["coin_100"]; ?><br>
+ coin_25 <?php echo $_POST["coin_25"]; ?><br>
+ coin_10 <?php echo $_POST["coin_10"]; ?><br>
+  coin_5 <?php echo $_POST["coin_5"]; ?><br>
+  coin_1 <?php echo $_POST["coin_1"]; ?><br>
+CountClose <?php echo $_POST["CountClose"]; ?><br>
+CountDelta <?php echo $_POST["CountDelta"]; ?><br>
+POS_Amount <?php echo $_POST["POS_Amount"]; ?><br>
+   POS_Tip <?php echo $_POST["POS_Tip"]; ?><br>
+   POS_Due <?php echo $_POST["POS_Due"]; ?><br>
 
 
 </body>
