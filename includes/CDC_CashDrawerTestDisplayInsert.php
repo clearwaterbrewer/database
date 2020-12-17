5dbc46dbfeb99bbb8b744a124eed23156d6d3c6c
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-17 09:09:14 -0500

    Update CDC_CashDrawerTestDisplayInsert.php

--- includes/CDC_CashDrawerTestDisplayInsert.php
+++ includes/CDC_CashDrawerTestDisplayInsert.php
@@ -3,6 +3,18 @@
 
 Date <?php echo $_GET["DateTimeCode"]; ?><br>
 CountOpen <?php echo $_GET["CountOpen"]; ?><br>
+bill_100 <?php echo $_GET["bill_100"]; ?><br>
+ bill_50 <?php echo $_GET["bill_50"]; ?><br>
+ bill_20 <?php echo $_GET["bill_20"]; ?><br>
+ bill_10 <?php echo $_GET["bill_10"]; ?><br>
+  bill_5 <?php echo $_GET["bill_5"]; ?><br>
+  bill_1 <?php echo $_GET["bill_1"]; ?><br>
+coin_100 <?php echo $_GET["coin_100"]; ?><br>
+ coin_25 <?php echo $_GET["coin_25"]; ?><br>
+ coin_10 <?php echo $_GET["coin_10"]; ?><br>
+  coin_5 <?php echo $_GET["coin_5"]; ?><br>
+  coin_1 <?php echo $_GET["coin_1"]; ?><br>
+
 
 </body>
 </html>
