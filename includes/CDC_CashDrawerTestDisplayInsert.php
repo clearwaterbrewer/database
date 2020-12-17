c032c587ad663d3219f181ee4d670a9c0ac47074
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-17 09:00:37 -0500

    Create CDC_CashDrawerTestDisplayInsert.php

---
+++ includes/CDC_CashDrawerTestDisplayInsert.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+Date <?php echo $_GET["DateTimeCode"]; ?><br>
+Initials <?php echo $_GET["Initials"]; ?><br>
+CountOpen <?php echo $_GET["CountOpen"]; ?><br>
+bill_100 <?php echo $_GET["bill_100"]; ?><br>
+ bill_50 <?php echo $_GET["bill_50"]; ?><br>
+ bill_20 <?php echo $_GET["bill_20"]; ?><br>
+ bill_10 <?php echo $_GET["bill_10"]; ?><br>
+  bill_5 <?php echo $_GET["bill_5"]; ?><br>
+  bill_1 <?php echo $_GET["bill_1"]; ?><br>
+coin_100 <?php echo $_GET["coin_100"]; ?><br>
+ coin_25 <?php echo $_GET["coin_25"]; ?><br>
+ coin_10 <?php echo $_GET["coin_10"]; ?><br>
+  coin_5 <?php echo $_GET["coin_5"]; ?><br>
+  coin_1 <?php echo $_GET["coin_1"]; ?><br>
+CountClose <?php echo $_GET["CountClose"]; ?><br>
+CountDelta <?php echo $_GET["CountDelta"]; ?><br>
+POS_Amount <?php echo $_GET["POS_Amount"]; ?><br>
+   POS_Tip <?php echo $_GET["POS_Tip"]; ?><br>
+   POS_Due <?php echo $_GET["POS_Due"]; ?><br>
+
+</body>
+</html>
+
