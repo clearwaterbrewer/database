967c93030dd165a338a954dfc1f5660faef103f3
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-17 10:52:23 -0500

    Update CDC_CashDrawerCountForm.php

--- CDC_CashDrawerCountForm.php
+++ CDC_CashDrawerCountForm.php
@@ -35,74 +35,74 @@ sec_session_start();
        <label for="DateTimeCode">DateTimeCode</label>
           <input type="text" class="form-control" name="DateTimeCode" value="<?php echo date('Y-m-d H:i:s'); ?>" required>
         <label for="CountOpen">Opening Count</label>
-          <input type='number' inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8"class='form-control' id="CountOpen" name="CountOpen" step="0.01" value="0.00" tabindex=1 >
+          <input type='number' inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8"class='form-control' id="CountOpen" name="CountOpen" step="0.01" value="0.00" tabindex=1 required>
        </div>
     </li>
 
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="bill_100"> $100 bills</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_100" name="bill_100" tabindex=2 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_100" name="bill_100" tabindex=2 value="0" required>
         <label for="coin_100"> $1 coins</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_100" name="coin_100" tabindex=8 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_100" name="coin_100" tabindex=8 value="0" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="bill_50"> $50 bills</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_50" name="bill_50" tabindex=3 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_50" name="bill_50" tabindex=3 value="0" required>
         <label for="coin_25"> quarters</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_25" name="coin_25" tabindex=9 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_25" name="coin_25" tabindex=9 value="0" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="bill_20"> $20 bills</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_20" name="bill_20" tabindex=4 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_20" name="bill_20" tabindex=4 value="0" required>
         <label for="coin_10"> __dimes</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_10" name="coin_10" tabindex=10 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_10" name="coin_10" tabindex=10 value="0" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="bill_10"> $10 bills</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_10" name="bill_10" tabindex=5 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_10" name="bill_10" tabindex=5 value="0" required>
         <label for="coin_5"> _nickels</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_5" name="coin_5" tabindex=11 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_5" name="coin_5" tabindex=11 value="0" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="bill_5"> $5 bills_</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_5" name="bill_5" tabindex=6 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_5" name="bill_5" tabindex=6 value="0" required>
         <label for="coin_1"> pennies</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_1" name="coin_1" tabindex=12 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="coin_1" name="coin_1" tabindex=12 value="0" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="bill_1"> $1 bills</label>
-          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_1" name="bill_1" tabindex=7 value="" >
+          <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_1" name="bill_1" tabindex=7 value="0" required>
         <label for="coinTotal"> coin total</label>
-          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="coinTotal" name="coinTotal" step="0.01" value="0" >
+          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="coinTotal" name="coinTotal" step="0.01" value="0" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="CountClose"> Closing Count</label>
-          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="CountClose" name="CountClose"step="0.01"  tabindex=13 value="0.00" >
+          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="CountClose" name="CountClose"step="0.01"  tabindex=13 value="0.00" required>
         <label for="CountDelta"> Difference</label>
-          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="CountDelta" name="CountDelta"step="0.01"  tabindex=14 value="0.00" >
+          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="CountDelta" name="CountDelta"step="0.01"  tabindex=14 value="0.00" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
         <div class="form-input-wide">
         <label for="POS_Amount"> POS_Amount </label>
-          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="POS_Amount" name="POS_Amount" tabindex=15 step="0.01" value="0.00" >
+          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="POS_Amount" name="POS_Amount" tabindex=15 step="0.01" value="0.00" required>
         <label for="POS_Tip"> POS_Tip </label>
-          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="POS_Tip" name="POS_Tip" tabindex=16 step="0.01" value="0.00" >
+          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="POS_Tip" name="POS_Tip" tabindex=16 step="0.01" value="0.00" required>
         <label for="POS_Due"> POS_Due </label>
-          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="POS_Due" name="POS_Due" tabindex=17 step="0.01" value="0.00" >
+          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="POS_Due" name="POS_Due" tabindex=17 step="0.01" value="0.00" required>
         </div>
     </li>
   </ul>
