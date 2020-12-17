b8d205abb895d76e886a7bba1f4905e80f786dbd
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2020-12-17 09:27:20 -0500

    Update CDC_CashDrawerTestDisplayInsert.php

--- includes/CDC_CashDrawerTestDisplayInsert.php
+++ includes/CDC_CashDrawerTestDisplayInsert.php
@@ -1,25 +1,30 @@
 <html>
 <body>
 
-Date <?php echo $_POST["DateTimeCode"]; ?><br>
-Initials <?php echo $_POST["Initials"]; ?><br>
-CountOpen <?php echo $_POST["CountOpen"]; ?><br>
-bill_100 <?php echo $_POST["bill_100"]; ?><br>
- bill_50 <?php echo $_POST["bill_50"]; ?><br>
- bill_20 <?php echo $_POST["bill_20"]; ?><br>
- bill_10 <?php echo $_POST["bill_10"]; ?><br>
-  bill_5 <?php echo $_POST["bill_5"]; ?><br>
-  bill_1 <?php echo $_POST["bill_1"]; ?><br>
-coin_100 <?php echo $_POST["coin_100"]; ?><br>
- coin_25 <?php echo $_POST["coin_25"]; ?><br>
- coin_10 <?php echo $_POST["coin_10"]; ?><br>
-  coin_5 <?php echo $_POST["coin_5"]; ?><br>
-  coin_1 <?php echo $_POST["coin_1"]; ?><br>
-CountClose <?php echo $_POST["CountClose"]; ?><br>
-CountDelta <?php echo $_POST["CountDelta"]; ?><br>
-POS_Amount <?php echo $_POST["POS_Amount"]; ?><br>
-   POS_Tip <?php echo $_POST["POS_Tip"]; ?><br>
-   POS_Due <?php echo $_POST["POS_Due"]; ?><br>
+INSERT INTO DrawerCounts<br>
+(DateTimeCode, Initials, CountOpen, bill_100, bill_50, bill_20, bill_10, bill_5, bill_1, coin_100, coin_25, coin_10, coin_5, coin_1, CountClose, CountDelta, POS_Amount, POS_Tip, POS_Due)<br>
+VALUES 
+(
+"<?php echo $_POST["DateTimeCode"]; ?>", 
+"<?php echo $_POST["Initials"]; ?>", 
+<?php echo $_POST["CountOpen"]; ?>, 
+<?php echo $_POST["bill_100"]; ?>, 
+<?php echo $_POST["bill_50"]; ?>, 
+<?php echo $_POST["bill_20"]; ?>, 
+<?php echo $_POST["bill_10"]; ?>, 
+<?php echo $_POST["bill_5"]; ?>, 
+<?php echo $_POST["bill_1"]; ?>, 
+<?php echo $_POST["coin_100"]; ?>, 
+<?php echo $_POST["coin_25"]; ?>, 
+<?php echo $_POST["coin_10"]; ?>, 
+<?php echo $_POST["coin_5"]; ?>, 
+<?php echo $_POST["coin_1"]; ?>, 
+<?php echo $_POST["CountClose"]; ?>, 
+<?php echo $_POST["CountDelta"]; ?>, 
+<?php echo $_POST["POS_Amount"]; ?>, 
+<?php echo $_POST["POS_Tip"]; ?>, 
+<?php echo $_POST["POS_Due"]; ?> 
+);<br>
 
 
 </body>
