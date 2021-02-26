aaa3999b3745824456618696de7b95e4587c409f
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2021-02-26 13:06:48 -0500

    Update CDC_CashDrawerCountForm.php

--- CDC_CashDrawerCountForm.php
+++ CDC_CashDrawerCountForm.php
@@ -152,7 +152,7 @@ sec_session_start();
 		    var bill5 = $('#bill_5').val();
 		    var bill1 = $('#bill_1').val();
 		    var coin = $('#coins').val();
-		    var CountClose = (bill100 * 100) + (bill50 * 50) + (bill20 * 20) + (bill10 * 10) + (bill5 * 5) + (bill1 * 1) + (coinTotal * 1);
+		    var CountClose = (bill100 * 100) + (bill50 * 50) + (bill20 * 20) + (bill10 * 10) + (bill5 * 5) + (bill1 * 1) + (coin * 1);
 		    $('#CountClose').val(CountClose);
 		    });
 	    
