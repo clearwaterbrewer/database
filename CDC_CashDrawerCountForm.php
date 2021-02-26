f3d3cd923cf2a6e4110c214136b3469391df8448
clearwaterbrewer <34481584+clearwaterbrewer@users.noreply.github.com>
2021-02-26 13:05:13 -0500

    Update CDC_CashDrawerCountForm.php

--- CDC_CashDrawerCountForm.php
+++ CDC_CashDrawerCountForm.php
@@ -84,7 +84,7 @@ sec_session_start();
         <label for="bill_1"> $1 bills</label>
           <input type="number" inputmode="numeric" pattern="^\d{1,3}$" size="4" class="form-control" id="bill_1" name="bill_1" tabindex=7 value="0" required>
         <label for="coinTotal"> coin total</label>
-          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="coinTotal" name="coinTotal" step="0.01" value="0" required>
+          <input type="number" inputmode="numeric" pattern="(\d{3})([\.])(\d{2})" size="8" class="form-control" id="coinTotal" name="coinTotal" step="0.01" value="0.00" required>
         </div>
     </li>
     <li class="form-line" data-type="control_textbox">
